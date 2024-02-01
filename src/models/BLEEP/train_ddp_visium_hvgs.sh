a607691350e298590731979a0ec8adb66cf20110
Outlier <86715543+uqer-jyh@users.noreply.github.com>
2024-02-01 13:30:45 +1000

    update files

--- src/models/BLEEP/train_ddp_visium_hvgs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python BLEEP_main.py --init_method tcp://$MASTER_ADDR:3456 --exp_name clip_visium_bc_6 --batch_size 256 --max_epochs 150 --num_workers 4 --fold $SLURM_ARRAY_TASK_ID --dim 6118 --data_name visium_bc_6subset
-
-
-
--- src/models/BLEEP/train_ddp_visium_markers.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python BLEEP_main.py --init_method tcp://$MASTER_ADDR:3456 --exp_name clip_visium_bc --batch_size 256 --max_epochs 150 --num_workers 4 --fold $SLURM_ARRAY_TASK_ID --dim 5689 --data_name visium_bc
